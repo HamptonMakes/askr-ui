634662d7a3e31960d5809577f58eed135c0bbb08
Hampton Catlin <hcatlin@gmail.com>
2015-03-26 13:46:45 -0400

    allow linking directly to articles

--- app/components/survey-question.js
+++ app/components/survey-question.js
@@ -4,6 +4,16 @@ import layout from '../templates/components/survey-question';
 export default Ember.Component.extend({
   layout: layout,
   showYearlyResults: false,
+  init: function() {
+    this._super();
+    if(window.top.location.hash === ("#" + this.get("question.id"))) {
+      this.set("showYearlyResults", true);
+    }
+  }.observes("question"),
+  permalink: function() {
+    var loc = window.location;
+    return (loc.origin + loc.pathname + "#" + this.get("question.id"));
+  }.property("question.id"),
   actions: {
     toggleResults: function() {
       this.toggleProperty("showYearlyResults");
