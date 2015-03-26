998162cca6d7aec8dcbaa4df73f1bc414f3a4651
Hampton Catlin <hcatlin@gmail.com>
2015-03-26 14:03:02 -0400

    hopefully permalinks will work with this...

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,7 +6,6 @@ export default Ember.Route.extend({
   actions: {
     didTransition: function() {
       if (ENV.environment === 'production') {
-        console.log("transition");
         Ember.run(function() {
           ga('send', 'pageview', {
             'page': window.location.pathname,
